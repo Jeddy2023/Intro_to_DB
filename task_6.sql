a010f04770a055c0b4bb302442e16aca36419853
jeddy2023 <jkenosuh@gmail.com>
2025-01-17 03:43:42 +0100

    checking

--- task_6.sql
+++ task_6.sql
@@ -1,2 +1,4 @@
 INSERT INTO customer, "124 Happiness  Ave."
 ["VALUES", "Blessing Malik", "bmalik@sandtech.com"]
+["Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave."]
+["Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness  Ave."]
